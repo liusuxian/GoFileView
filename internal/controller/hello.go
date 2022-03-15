02d65b74c196392c1e3e17241aa66291ed1f0f95
liusuxian <382185882@qq.com>
2022-03-15 10:26:55 +0800

    update

--- internal/controller/hello.go
+++ internal/controller/hello.go
@@ -4,6 +4,7 @@ import (
 	"GoFileView/api/v1"
 	"GoFileView/utility/logger"
 	"GoFileView/utility/response"
+	"GoFileView/utility/utils"
 	"context"
 )
 
@@ -15,6 +16,9 @@ type cHello struct{}
 
 func (c *cHello) Hello(ctx context.Context, req *v1.HelloReq) (res *v1.HelloRes, err error) {
 	logger.Print(ctx, "Hello req：", req)
+	resultPath := utils.ConvertToPDF("test.pptx")
+	utils.ConvertToImg(resultPath)
+	utils.WaterMark(resultPath, "")
 	response.JsonOK(ctx, "Hello World!")
 	return
 }
