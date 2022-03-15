2afc7187f0c6340cd02c05c5eb4348b5c74b661f
liusuxian <382185882@qq.com>
2022-03-15 15:46:33 +0800

    update

--- api/v1/view.go
+++ api/v1/view.go
@@ -4,11 +4,11 @@ import "github.com/gogf/gf/v2/frame/g"
 
 type ViewReq struct {
 	g.Meta    `path:"/view" tags:"view" method:"post" summary:"You first view api"`
-	Url       string `json:"Url"`
-	Type      string `json:"Type"`      // 判断是图片展示，还是pdf展示
-	FileWay   string `json:"FileWay"`   // 判断是否是本地文件
-	WaterMark string `json:"watermark"` // 水印
+	Url       string `json:"Url" dc:"文件url地址"`
+	Type      string `json:"Type" v:"pdf、\"\"" dc:"判断是图片展示，还是pdf展示"` // 判断是图片展示，还是pdf展示
+	FileWay   string `json:"FileWay" v:"local、\"\"" dc:"判断是否是本地文件"`  // 判断是否是本地文件
+	WaterMark string `json:"watermark" dc:"水印内容"`                    // 水印内容
 }
 type ViewRes struct {
-	g.Meta `mime:"text/html" example:"string"`
+	g.Meta `mime:"text/html" type:"string" example:"<html/>"`
 }
