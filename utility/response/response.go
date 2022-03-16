b594c62e962f9f2db9ab6c7ed8d1376666cfddaf
liusuxian <382185882@qq.com>
2022-03-17 00:10:36 +0800

    update

--- utility/response/response.go
+++ utility/response/response.go
@@ -10,30 +10,30 @@ import (
 
 // JsonResponse 数据返回通用JSON数据结构
 type JsonResponse struct {
-	Code    int         `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
-	Message string      `json:"message"` // 提示信息
-	Data    interface{} `json:"data"`    // 返回数据(业务接口定义具体数据结构)
+	Code    int    `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
+	Message string `json:"message"` // 提示信息
+	Data    any    `json:"data"`    // 返回数据(业务接口定义具体数据结构)
 }
 
 // JsonOK 标准返回结果数据结构封装。
-func JsonOK(ctx context.Context, data interface{}) {
+func JsonOK(ctx context.Context, data any) {
 	Json(ctx, gcode.CodeOK.Code(), gcode.CodeOK.Message(), data)
 }
 
 // JsonExit 返回JSON数据并退出当前HTTP执行函数。
 func JsonExit(ctx context.Context, code int, message string) {
 	Json(ctx, code, message, nil)
-	g.Throw("exit")
+	//g.Throw("exit")
 }
 
 // JsonResExit 返回JSON数据并退出当前HTTP执行函数。
-func JsonResExit(ctx context.Context, code int, message string, data interface{}) {
+func JsonResExit(ctx context.Context, code int, message string, data any) {
 	Json(ctx, code, message, data)
 	g.Throw("exit")
 }
 
 // Json 标准返回结果数据结构封装。
-func Json(ctx context.Context, code int, message string, data interface{}) {
+func Json(ctx context.Context, code int, message string, data any) {
 	err := g.RequestFromCtx(ctx).Response.WriteJson(JsonResponse{
 		Code:    code,
 		Message: message,
